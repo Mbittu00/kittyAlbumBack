dfe88e83e8e086ff171ed11becc7474cd341f5f3
mbittu000 <mbittu000@gmail.com>
2022-07-19 19:50:26 +0530

    te

--- index.js
+++ index.js
@@ -11,9 +11,6 @@ let port=process.env.PORT || 8080
 app.use(express.json())
 app.use(cors())
 //routes 
-app.get('/',(req,res)=>{
-  res.send('hello world')
-})
 app.use('/auth',auth)
 app.use('/img',img)
 //listen
